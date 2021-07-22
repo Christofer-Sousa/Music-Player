64ed4db116dde951a67829bf2f607d79e7939371
Christofer <christofersousa.js@gmail.com>
2021-07-22 11:09:38 -0300

    files

---
+++ src/music_list.js
@@ -0,0 +1,45 @@
+const songs_list = [
+    {
+        name: "Ant Saunders - Yellow Hearts" ,
+        path: "./src/audios/Ant Saunders - Yellow Hearts LEGENDADO TRADUÇÃO - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "NoMBe - Summer s Gone" ,
+        path: "./src/audios/NoMBe - Summer s Gone (Official Music Video) ft. Thutmose - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "Washington jr - Just the two of us" ,
+        path: "./src/audios/grover washington jr - just the two of us (TikTok Remix) [Lyrics] - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "JERHELL - Soul Food" ,
+        path: "./src/audios/JERHELL - Soul Food (Prod. by JERHELL) - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "Neffex - Rumours" ,
+        path: "./src/audios/neffexrumours [Legendado] - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "NoMBe - California Girls" ,
+        path: "./src/audios/NoMBe - California Girls (Official Music Video) - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "Ollie - Stuck" ,
+        path: "./src/audios/Ollie - Stuck - oDownloader.com.mp3",
+        image: ""
+    },
+    {
+        name: "Plvtinumcome my way",
+        path: "./src/audios/plvtinumcome my way [Legendado] - oDownloader.com.mp3",
+        image: ""
+    }
+
+]
+export default songs_list;
+
